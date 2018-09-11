5731e0db3a9571a6ad18fac187be5243b1d7d710
RyanPerkins <bobefett.qwert12@gmail.com>
2018-09-11 08:04:09 -0600

    made a model package with default MarshmallowMonster class

---
+++ src/model/MarshmallowMonster.java
@@ -0,0 +1,15 @@
+package model;
+
+public class MarshmallowMonster
+{
+	public MarshmallowMonster()
+	{
+
+	}
+
+	public String toString()
+	{
+		String description = "";
+		return description;
+	}
+}
