20126465d9d9ac8dd96afe121423c261d9ec2abc
RyanPerkins <bobefett.qwert12@gmail.com>
2018-09-11 09:26:04 -0600

    filled out the private variables, the constructor & the toString method

--- src/controller/MonsterController.java
+++ src/controller/MonsterController.java
@@ -1,6 +1,18 @@
 package controller;
 
+import model.MarshmallowMonster;
+
 public class MonsterController
 {
+	private MarshmallowMonster myMonster;
 
+	public MonsterController()
+	{
+		myMonster = new MarshmallowMonster("bobby", 3.5, 3, true, 1);
+	}
+
+	public void start()
+	{
+		System.out.println(myMonster);
+	}
 }
--- src/model/MarshmallowMonster.java
+++ src/model/MarshmallowMonster.java
@@ -2,14 +2,32 @@ package model;
 
 public class MarshmallowMonster
 {
+	private String name;
+	private double legCount;
+	private int eyeCount;
+	private boolean hasNoses;
+	private int armCount;
+
 	public MarshmallowMonster()
 	{
 
 	}
 
+	public MarshmallowMonster(String name, double legCount, int eyeCount, boolean hasNoses, int armCount)
+	{
+		this.name = name;
+		this.legCount = legCount;
+		this.eyeCount = eyeCount;
+		this.hasNoses = hasNoses;
+		this.armCount = armCount;
+	}
+
 	public String toString()
 	{
-		String description = "";
+		String description = "My monster's name is " + name
+				+ ". He has " + legCount + " legs, "
+				+ eyeCount + " eyes, and "
+				+ armCount + " arms.";
 		return description;
 	}
 }
